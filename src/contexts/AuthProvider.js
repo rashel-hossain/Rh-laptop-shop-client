1010649e3d741dc83807d3a25a5068f46110d837
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-24 01:09:29 +0600

    initial commit and file folder setup

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react';
+
+
+export const AuthContext = createContext();
+
+
+const AuthProvider = ({ children }) => {
+
+
+
+    return (
+        <div>
+            <AuthContext.Provider>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
