1338710da90520776a4b093bcb375d3b9ca480de
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-12-22 17:31:53 +0600

    meeting with client and seller host admin

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AdvertisedProducts from '../AdvertisedProducts/AdvertisedProducts';
 import Banner from '../Banner/Banner';
+import ClientMeeting from '../ClientMeeting/ClientMeeting';
 import ContactForm from '../ContactUs/ContactForm';
 import HomeCategories from '../HomeCategories/HomeCategories';
 import Reviews from '../UserReview/Reviews';
@@ -11,6 +12,7 @@ const Home = () => {
             <Banner></Banner>
             <AdvertisedProducts></AdvertisedProducts>
             <HomeCategories></HomeCategories>
+            <ClientMeeting></ClientMeeting>
             <Reviews></Reviews>
             <ContactForm></ContactForm>
         </div>
